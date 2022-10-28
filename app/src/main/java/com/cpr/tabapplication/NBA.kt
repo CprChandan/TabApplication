9fc414fdc705b029547f56e485a84345a0820835
CprChandan <chandraprakashr@gmail.com>
2022-10-28 10:40:06 +0530

    First Commit

---
+++ app/src/main/java/com/cpr/tabapplication/NBA.kt
@@ -0,0 +1,17 @@
+package com.cpr.tabapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class NBA : Fragment() {
+   override fun onCreateView(
+      inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?
+   ): View? {
+      // Inflate the layout for this fragment
+      return inflater.inflate(R.layout.fragment_n_b_a, container, false)
+   }
+}
